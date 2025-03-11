a6738ae0750be8c6c13abdf7ec5ceaa54adb1d7a
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-03-11 20:52:49 +0545

    minor config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}","./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
-      colors:{
-        primary:"#030014"
-      }
+      colors: {
+        primary: "#030014",
+        accent: "#B299FF",
+      },
     },
   },
   plugins: [],
-}
+};
