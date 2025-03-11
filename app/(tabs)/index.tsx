0dee757e35dbfc26e6090dd297caaac6be55da2c
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-03-11 20:53:42 +0545

    minor change

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -48,7 +48,7 @@ export default function Index() {
           <View className="flex-1 mt-5">
             <SearchBar
               onPress={() => router.push("/search")}
-              placeholder="Search for a movie"
+              placeholder="Search for a movies..."
             />
             <>
               <Text className="text-lg text-white font-bold mt-5 mb-3">
